0a6f0f2458d4269979b96ea5f985b05bda01bc70
SenorLawyer <larsmutsaers2007@gmail.com>
2024-06-19 14:00:19 +0200

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,17 @@
 import * as net from "net";
 
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.end();
+
   socket.on("data", (data) => {
     const request = data.toString();
     const path = request.split(" ")[1];
 
     const response =
       path.startsWith("/") && path.length > 1
-        ? "HTTP/1.1 200 OK\r\n\r\n"
-        : "HTTP/1.1 404 Not Found\r\n\r\n";
+        ? "HTTP/1.1 404 Not Found\r\n\r\n"
+        : "HTTP/1.1 200 OK\r\n\r\n";
 
     socket.write(response);
     socket.end();
