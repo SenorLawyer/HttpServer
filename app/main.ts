c7eed0a6c2575a3bed26063dbc5b649d57827622
SenorLawyer <larsmutsaers2007@gmail.com>
2024-06-19 14:32:05 +0200

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -23,64 +23,87 @@ const server = net.createServer((socket) => {
       socket.end();
     }
 
-    if (method !== "GET") {
-      response = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
-      changeResponse(response);
-      return;
-    }
+    if (method === "GET") {
+      switch (parts[1]) {
+        case "": {
+          response = "HTTP/1.1 200 OK\r\n\r\n";
+          changeResponse(response);
+          break;
+        }
 
-    switch (parts[1]) {
-      case "": {
-        response = "HTTP/1.1 200 OK\r\n\r\n";
-        changeResponse(response);
-        break;
+        case "echo": {
+          const message = parts[2];
+          response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
+          changeResponse(response);
+          break;
+        }
+
+        case "user-agent": {
+          const userAgent = request.split("User-Agent: ")[1].split("\r\n")[0];
+          response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+          changeResponse(response);
+          break;
+        }
+
+        case "files": {
+          const filename = parts.slice(2).join("/");
+          const filePath = path.join(filesDirectory, filename);
+
+          fs.stat(filePath, (err, stats) => {
+            if (err || !stats.isFile()) {
+              response = "HTTP/1.1 404 Not Found\r\n\r\n";
+              changeResponse(response);
+              return;
+            }
+
+            fs.readFile(filePath, (err, content) => {
+              if (err) {
+                response = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+                changeResponse(response);
+                return;
+              }
+
+              response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n`;
+              socket.write(response);
+              socket.write(content);
+              socket.end();
+            });
+          });
+          break;
+        }
+
+        default: {
+          response = "HTTP/1.1 404 Not Found\r\n\r\n";
+          changeResponse(response);
+          break;
+        }
       }
+    } else if (method === "POST") {
+      switch (parts[1]) {
+        case "files": {
+          const filename = parts.slice(2).join("/");
+          const filePath = path.join(filesDirectory, filename);
+          const body = request.split("\r\n\r\n")[1];
 
-      case "echo": {
-        const message = parts[2];
-        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
-        changeResponse(response);
-        break;
-      }
-
-      case "user-agent": {
-        const userAgent = request.split("User-Agent: ")[1].split("\r\n")[0];
-        response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-        changeResponse(response);
-        break;
-      }
-
-      case "files": {
-        const filename = parts.slice(2).join("/");
-        const filePath = path.join(filesDirectory, filename);
-
-        fs.stat(filePath, (err, stats) => {
-          if (err || !stats.isFile()) {
-            response = "HTTP/1.1 404 Not Found\r\n\r\n";
-            changeResponse(response);
-            return;
-          }
-
-          fs.readFile(filePath, (err, content) => {
+          fs.writeFile(filePath, body, (err) => {
             if (err) {
               response = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
               changeResponse(response);
               return;
             }
 
-            response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n`;
-            socket.write(response);
-            socket.write(content);
-            socket.end();
+            response = "HTTP/1.1 201 Created\r\n\r\n";
+            changeResponse(response);
           });
-        });
-        break;
-      }
 
-      default: {
-        response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        changeResponse(response);
-        break;
+          break;
+        }
+
+        default: {
+          response = "HTTP/1.1 404 Not Found\r\n\r\n";
+          changeResponse(response);
+          break;
+        }
       }
     }
   });
