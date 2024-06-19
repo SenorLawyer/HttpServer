cd20f6f5faef69f14f423f9b8563f51d3350165e
SenorLawyer <larsmutsaers2007@gmail.com>
2024-06-19 13:51:13 +0200

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,19 @@
 import * as net from "net";
 
 const server = net.createServer((socket) => {
-  console.log(socket.remoteAddress, "connected");
-  socket.write("HTTP/1.1 404 OK\r\n\r\n");
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
   socket.end();
+
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const path = request.split(" ")[1];
+    const response = path === "/" ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    socket.write(response);
+
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost");
