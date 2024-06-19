d61ddf9e9c7caaf8a23facd9226aa89e5a2e8fe3
SenorLawyer <larsmutsaers2007@gmail.com>
2024-06-19 13:52:40 +0200

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -8,12 +8,12 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     const path = request.split(" ")[1];
+
     const response = path === "/" ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
 
     socket.write(response);
-
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => console.log("Server is running on port 4221"));
